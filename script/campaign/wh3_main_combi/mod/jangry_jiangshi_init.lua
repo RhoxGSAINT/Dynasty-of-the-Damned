a92a0a3f467d03e1231ccabc96d787c0d84f46d4
RhoxGSAINT <swordknight@naver.com>
2023-09-10 18:24:34 +0900

    Some changes
    Deleted the master necromancer art set as it wasn't using and it was overriding the vanilla with the blank
    Changed the initial summon position slightly and changed the initial facing opponent's faction
    Destroyed unaccessible building in the bridge of heaven (forest chain)

--- script/campaign/wh3_main_combi/mod/jangry_jiangshi_init.lua
+++ script/campaign/wh3_main_combi/mod/jangry_jiangshi_init.lua
@@ -18,6 +18,10 @@ local function list_to_string(list)
 end
 
 local function jiangshi_init()
+    pcall(function()
+        mixer_set_faction_trait("wh3_dlc21_vmp_jiangshi_rebels", "jiangshi_rebels_bundle", true)
+    end)
+    
     if cm:is_new_game() then
  		
 		cm:disable_event_feed_events(true, "wh_event_category_character", "", "");	
@@ -31,11 +35,19 @@ local function jiangshi_init()
 		
 		cm:callback(function() cm:disable_event_feed_events(false, "wh_event_category_character", "", "") end, 1);	
         
+        local bridge_of_heaven_region = cm:get_region("wh3_main_combi_region_bridge_of_heaven")
+        local bridge_of_heaven_slot_list = bridge_of_heaven_region:settlement():slot_list()
+        for i = 1, bridge_of_heaven_slot_list:num_items() - 1 do
+            local slot = bridge_of_heaven_slot_list:item_at(i)
+            cm:instantly_dismantle_building_in_region(slot)
+        end
+        
+        
         local jiangshi_faction_str = "wh3_dlc21_vmp_jiangshi_rebels";
 		local jiangshi_unit_list = "jiang_grave_guard_halberd,jiang_zombie_longspear,jiang_zombie_longspear,jiang_zombie_longspear,jiang_zombie_longspear,jiang_shadow_lantern,jiang_shuimu";
         local jiangshi_region_key = "wh3_main_combi_region_nonchang";
-        local jiangshi_x_pos = 1271;
-        local jiangshi_y_pos = 490;
+        local jiangshi_x_pos = 1265;
+        local jiangshi_y_pos = 480;
         local jiangshi__type = "general";
         local jiangshi__subtype = "jiang_xiu_necro";
         local jiangshi_forename = "names_name_778301541";
@@ -63,7 +75,7 @@ local function jiangshi_init()
                 cm:set_character_immortality(jiangshi_char_str, true)
                 cm:set_character_unique(jiangshi_char_str, true)
                 --cm:add_agent_experience(jiangshi_char_str, 1000)
-                mixer_set_faction_trait("wh3_dlc21_vmp_jiangshi_rebels", "jiangshi_rebels_bundle", true)
+                
             end
         );
         out("JANGRY - Created jiangshi");  
@@ -80,7 +92,7 @@ local function jiangshi_init()
 
         if jiangshi_faction:is_human() then     
             
-            local jiangshi_enemy_faction_str = "wh3_main_cth_burning_wind_nomads";
+            local jiangshi_enemy_faction_str = "wh3_main_cth_the_jade_custodians";
             local jiangshi_enemy_unit_list = "wh3_main_cth_inf_jade_warriors_1","wh3_main_cth_inf_peasant_spearmen_1","wh3_main_cth_inf_peasant_spearmen_1","wh3_main_cth_inf_peasant_archers_0","wh3_main_cth_inf_jade_warriors_0","wh3_main_cth_inf_jade_warriors_0","wh3_main_cth_cav_peasant_horsemen_0";
             local jiangshi_enemy_region_key = "wh3_main_combi_region_nonchang";
             local jiangshi_enemy_x_pos = 1269;
@@ -108,15 +120,15 @@ local function jiangshi_init()
                 jiangshi_enemy_surname,
                 jiangshi_enemy_othername,
                 jiangshi_enemy_is_faction_leader,
-                    function(cqi, mf_cqi)
-                        jiangshi_enemy_char_str = cm:char_lookup_str(cqi);
-                        jiangshi_enemy_army_cqi = mf_cqi
-                        cm:set_force_has_retreated_this_turn(cm:get_military_force_by_cqi(jiangshi_enemy_army_cqi))
-                        out(char_str)
-                    end
-                );
+                function(cqi, mf_cqi)
+                    jiangshi_enemy_char_str = cm:char_lookup_str(cqi);
+                    jiangshi_enemy_army_cqi = mf_cqi
+                    cm:set_force_has_retreated_this_turn(cm:get_military_force_by_cqi(jiangshi_enemy_army_cqi))
+                    out(char_str)
+                end
+            );
                 out("JANGRY - Created jiangshi Enemy");
-
+            cm:force_declare_war("wh3_dlc21_vmp_jiangshi_rebels", jiangshi_enemy_faction_str, false, false)
 --- How They Play event and first init mission would go here ---
             cm:callback(
                 function()
